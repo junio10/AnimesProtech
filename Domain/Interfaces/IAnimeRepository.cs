e6c1ba291f8574347f8782ceae9ee9110490b743
junio10 <junio.js977@gmail.com>
2024-10-04 21:45:14 -0300

    feat: adicionando Interface director e operações junto com alteracoes na interface anime

--- Domain/Interfaces/IAnimeRepository.cs
+++ Domain/Interfaces/IAnimeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace Domain.Interfaces
 {
-    internal interface IAnimeRepository
+    public interface IAnimeRepository
     {
+        Task<Anime> add(Anime anime);
+        Task<Anime> update(Anime anime);
+        Task<Anime> delete(int id);
+        Task<Anime> getAnimeByName(String name);
+        Task<IEnumerable<Anime>> getAnimesByKeyWords(String summary);
+        Task<IEnumerable<Anime>> animesByIdDirector(int id);
+
+
+
+
+
     }
 }
---
+++ Domain/Interfaces/IDirectorRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IDirectorRepository
+    {
+        Task<Director> add(Director director);
+        Task<Director> update(int id);        
+        Task<Director> getDirectorByName(String name);
+
+        Task<IEnumerable<Director>> getAllDirector();
+
+    }
+}
