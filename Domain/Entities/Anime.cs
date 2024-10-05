afc4c593c22f551c0cb1890f7a7c2f78e95300c7
junio10 <junio.js977@gmail.com>
2024-10-04 21:06:54 -0300

    feat: adicionadas as pastas de entidades e interfaces, classe anime e diretor

---
+++ Domain/Entities/Anime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Anime
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Summary { get; set; }
+
+        public Director Director { get; set; } = null!;
+
+        public int DirectorId { get; set; }
+    }
+}
---
+++ Domain/Entities/Director.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Director
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public List<Anime>? Animes { get; set; }
+    }
+}
