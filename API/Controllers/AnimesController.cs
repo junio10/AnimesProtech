82795b3ae1287fd3d787bc8e7d2cea8b57f57fb5
junio10 <junio.js977@gmail.com>
2024-10-05 09:20:23 -0300

    feat: criacao da Controllers/AnimesController.cs

---
+++ API/Controllers/AnimesController.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Infraestructure.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AnimesController : ControllerBase
+{
+    private readonly ProtechAnimesContext _context;
+    public AnimesController(ProtechAnimesContext context) {
+        _context = context;
+    }
+    public IEnumerable<Anime> Index()
+    {
+       return _context.Animes.ToList();
+    }
+}
