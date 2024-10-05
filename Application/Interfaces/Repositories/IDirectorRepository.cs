178c2a4ee25f6bcc337a5b65dc7da7cfa028cecc
junio10 <junio.js977@gmail.com>
2024-10-05 09:19:59 -0300

    feat: criacao da Interfaces/Repositories/IDirectorRepository

---
+++ Application/Interfaces/Repositories/IDirectorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IDirectorRepository
+    {
+        Task<Director> add(Director director);
+        Task<Director> update(int id);
+        Task<Director> getDirectorByName(string name);
+
+        Task<IEnumerable<Director>> getAllDirector();
+
+    }
+}
