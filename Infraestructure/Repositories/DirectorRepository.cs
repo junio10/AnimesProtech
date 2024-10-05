3dc4014a8fcd4828f449446f8daab7e90aa1e7b7
junio10 <junio.js977@gmail.com>
2024-10-05 09:18:59 -0300

    feat: criacao de Repositories/DirectorRepository.cs

---
+++ Infraestructure/Repositories/DirectorRepository.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories;
+
+public class DirectorRepository : IDirectorRepository
+{
+    public Task<Director> add(Director director)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Director>> getAllDirector()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Director> getDirectorByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Director> update(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
