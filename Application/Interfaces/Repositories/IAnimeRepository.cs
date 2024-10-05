4f1a938b60eb98058adc10a931f1094afc41abb4
junio10 <junio.js977@gmail.com>
2024-10-05 09:13:43 -0300

    feat: criacao da interface animeRepository

---
+++ Application/Interfaces/Repositories/IAnimeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IAnimeRepository
+    {
+        Task<Anime> add(Anime anime);
+        Task<Anime> update(Anime anime);
+        Task<Anime> delete(int id);
+        Task<Anime> getAnimeByName(string name);
+        Task<IEnumerable<Anime>> getAnimesByKeyWords(string summary);
+        Task<IEnumerable<Anime>> animesByIdDirector(int id);
+
+
+
+
+
+    }
+}
