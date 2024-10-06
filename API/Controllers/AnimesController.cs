170e6b9ed54eac13dca5e6db67e4c77f35e52e79
junio10 <junio.js977@gmail.com>
2024-10-06 13:12:30 -0300

    refactor: add metódos de index e Create

--- API/Controllers/AnimesController.cs
+++ API/Controllers/AnimesController.cs
@@ -1,6 +1,9 @@
-﻿using Domain.Entities;
+﻿using Application.Features.AnimeFeature;
+using Domain.Entities;
 using Infraestructure.Context;
+using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -9,12 +12,24 @@ namespace API.Controllers;
 [ApiController]
 public class AnimesController : ControllerBase
 {
-    private readonly ProtechAnimesContext _context;
-    public AnimesController(ProtechAnimesContext context) {
-        _context = context;
+    private readonly IMediator _mediator;
+    public AnimesController(IMediator mediator) {
+        _mediator = mediator;
     }
-    public IEnumerable<Anime> Index()
+    [HttpGet("getAnimes")]
+    public async Task<GetAnimesResponse> Index()
     {
-       return _context.Animes.ToList();
+       return await  _mediator.Send(new GetAnimesQuery());
     }
+    [HttpPost("create")]
+    public async Task<IActionResult> Create([FromBody] CreateAnimeCommand request)
+    {
+        var anime = await _mediator.Send(request);
+        if (anime.anime is null) {
+            return BadRequest(new { Message = "Não foi possível criar o anime." });
+        }
+        return Ok(anime);
+    }
+
+
 }
