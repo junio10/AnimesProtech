6f1366a20de5798f11acd11d03cb0b34b3a5a2c8
junio10 <junio.js977@gmail.com>
2024-10-06 13:15:33 -0300

    feat: criando a UpdateDirectorFeature.cs

---
+++ Application/Features/DirectorFetures/UpdateDirectorFeature.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.DirectorFetures;
+
+public record UpdateDirectorCommand(
+    string name
+    ) : IRequest<UpdateDirectorResponse>;
+
+public record UpdateDirectorResponse(Director Director);
+
+public class UpdateDirectorHandler : IRequestHandler<UpdateDirectorCommand, UpdateDirectorResponse>
+{
+    private readonly IDirectorRepository _directorRepository;
+    public UpdateDirectorHandler(IDirectorRepository directorRepository)
+    {
+        _directorRepository = directorRepository;
+    }
+
+    public async Task<UpdateDirectorResponse> Handle(UpdateDirectorCommand request, CancellationToken cancellationToken)
+    {
+        Director director = new Director { Name = request.name };
+        var newAnime = await _directorRepository.Update(director);
+        return new UpdateDirectorResponse(newAnime);
+    }
+}
+
