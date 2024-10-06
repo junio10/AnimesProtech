e5364cce6b8d9a894b3d9a657ad6281a2866e23e
junio10 <junio.js977@gmail.com>
2024-10-06 09:27:22 -0300

    feat: adicionando classe GetAnimesByKeyWordsFeature.cs que faz a comunicacao entre Api e Infraestructure

---
+++ Application/Features/AnimeFeatures/GetAnimesByKeyWordsFeature.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.AnimeFeature;
+
+public record GetAnimesByKeyWordsQuery(string summary) : IRequest<GetAnimesByKeyWordsResponse>;
+
+public record GetAnimesByKeyWordsResponse(IEnumerable<Anime> Animes);
+
+public class GetAnimesByKeyWordsHandler : IRequestHandler<GetAnimesByKeyWordsQuery, GetAnimesByKeyWordsResponse>
+{
+    private readonly IAnimeRepository _animeRepository;
+    public GetAnimesByKeyWordsHandler(IAnimeRepository animeRepository)
+    {
+        _animeRepository = animeRepository;
+    }
+
+    public async Task<GetAnimesByKeyWordsResponse> Handle(GetAnimesByKeyWordsQuery request, CancellationToken cancellationToken)
+    {
+
+        var anime = await _animeRepository.GetAnimesByKeyWords(request.summary);
+        if (anime is null)
+        {
+            throw new ArgumentNullException(nameof(request));
+        }
+        return new GetAnimesByKeyWordsResponse(anime);
+    }
+}
+
