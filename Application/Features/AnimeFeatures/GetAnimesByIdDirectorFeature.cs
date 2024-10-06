b67defb196f5cb297c109ba751f1359174ca32fa
junio10 <junio.js977@gmail.com>
2024-10-06 09:26:47 -0300

    feat: adicionando classe GetAnimesByIdDirectorFeature.cs que faz a comunicacao entre Api e Infraestructure

---
+++ Application/Features/AnimeFeatures/GetAnimesByIdDirectorFeature.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.AnimeFeature;
+
+public record GetAnimesByIdDirectorCommand(
+    int idDiretor
+    ) : IRequest<GetAnimesByIdDirectorResponse>;
+
+public record GetAnimesByIdDirectorResponse(IEnumerable<Anime> Animes);
+
+public class GetAnimesByIdDirectorHandler : IRequestHandler<GetAnimesByIdDirectorCommand, GetAnimesByIdDirectorResponse>
+{
+    private readonly IAnimeRepository _animeRepository;
+    public GetAnimesByIdDirectorHandler(IAnimeRepository animeRepository)
+    {
+        _animeRepository = animeRepository;
+    }
+
+    public async Task<GetAnimesByIdDirectorResponse> Handle(GetAnimesByIdDirectorCommand request, CancellationToken cancellationToken)
+    {
+        var newAnime = await _animeRepository.AnimesByIdDirector(request.idDiretor);
+        return new GetAnimesByIdDirectorResponse(newAnime);
+    }
+}
