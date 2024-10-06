7f9ca6615bcc19a5ca890bc0f7a17b0adb88783d
junio10 <junio.js977@gmail.com>
2024-10-06 09:33:30 -0300

    feat: adicionando DependeceInjection em Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,16 @@
 using Infraestructure;
+using Application;
+using API;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfra(builder.Configuration);
-
+builder.Services.AddApplication();
+builder.Services.AddApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
