69d68850d8f931e8e3bac1b906f6dac3134c3d1f
junio10 <junio.js977@gmail.com>
2024-10-06 09:29:49 -0300

    feat: adicionando classe UpdateAnimeFeature que faz a comunicacao entre Api e Infraestructure

---
+++ Application/Features/AnimeFeatures/UpdateAnimeFeature.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.AnimeFeature;
+
+public record UpdateAnimeCommand(
+    string name,
+    string summary,
+    int idDiretor
+    ) : IRequest<UpdateAnimeResponse>;
+
+public record UpdateAnimeResponse(Anime anime);
+
+public class UpdateAnimeHandler : IRequestHandler<UpdateAnimeCommand, UpdateAnimeResponse>
+{
+    private readonly IAnimeRepository _animeRepository;
+    public UpdateAnimeHandler(IAnimeRepository animeRepository)
+    {
+        _animeRepository = animeRepository;
+    }
+
+    public async Task<UpdateAnimeResponse> Handle(UpdateAnimeCommand request, CancellationToken cancellationToken)
+    {
+        Anime anime = new Anime() { Name = request.name, Summary = request.summary, DirectorId = request.idDiretor };
+        var newAnime = await _animeRepository.Update(anime);
+        return new UpdateAnimeResponse(newAnime);
+    }
+}
+
