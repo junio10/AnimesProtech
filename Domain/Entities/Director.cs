cbf073df30529f968149c9a9f1946c20ec2e6946
junio10 <junio.js977@gmail.com>
2024-10-06 19:52:25 -0300

    refactor: adicionando o jsonIgnore para não criar loops

--- Domain/Entities/Director.cs
+++ Domain/Entities/Director.cs
@@ -1,10 +1,12 @@
-﻿namespace Domain.Entities;
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Entities;
 
 public class Director
 {
     public int Id { get; set; }
 
     public required string Name { get; set; }
-
+    [JsonIgnore]
     public List<Anime>? Animes { get; set; }
 }
