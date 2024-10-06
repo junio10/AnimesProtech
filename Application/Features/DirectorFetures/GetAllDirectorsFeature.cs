ba18a3efd8b7ceef30140c8d9f18d7e11ef8ac29
junio10 <junio.js977@gmail.com>
2024-10-06 09:31:37 -0300

    feat: adicionando classe GetAllDirectors que faz a comunicacao entre Api e Infraestructure

---
+++ Application/Features/DirectorFetures/GetAllDirectorsFeature.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.DirectorFetures;
+
+public record GetAllDirectorsCommand(
+    ) : IRequest<GetAllDirectorsResponse>;
+
+public record GetAllDirectorsResponse(IEnumerable<Director> Director);
+
+public class GetAllDirectorsHandler : IRequestHandler<GetAllDirectorsCommand, GetAllDirectorsResponse>
+{
+    private readonly IDirectorRepository _directorRepository;
+    public GetAllDirectorsHandler(IDirectorRepository directorRepository)
+    {
+        _directorRepository = directorRepository;
+    }
+
+    public async Task<GetAllDirectorsResponse> Handle(GetAllDirectorsCommand request, CancellationToken cancellationToken)
+    {
+       
+        var newAnime = await _directorRepository.GetAllDirectors();
+        return new GetAllDirectorsResponse(newAnime);
+    }
+}
