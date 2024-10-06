03b8c142736896dd0d5ac1e9b0fa0d32978d3e9e
junio10 <junio.js977@gmail.com>
2024-10-06 19:50:36 -0300

    refactor: adicionando delete, update, GetAnimesByKeyWords, GetAnimeByName, GetAnimeById

--- API/Controllers/AnimesController.cs
+++ API/Controllers/AnimesController.cs
@@ -1,10 +1,13 @@
 ﻿using Application.Features.AnimeFeature;
+using Application.Features.AnimeFeatures;
 using Domain.Entities;
+using Domain.Exceptions;
 using Infraestructure.Context;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace API.Controllers;
 
@@ -13,23 +16,83 @@ namespace API.Controllers;
 public class AnimesController : ControllerBase
 {
     private readonly IMediator _mediator;
-    public AnimesController(IMediator mediator) {
+    public AnimesController(IMediator mediator)
+    {
         _mediator = mediator;
     }
-    [HttpGet("getAnimes")]
-    public async Task<GetAnimesResponse> Index()
+    [HttpGet("Index")]
+    public async Task<IActionResult> Index()
     {
-       return await  _mediator.Send(new GetAnimesQuery());
+        var anime = await _mediator.Send(new GetAnimesQuery());
+        if (!anime.animes.Any())
+        {
+            return NotFound(new { Message = "Não existe anime" });
+        }
+
+        return Ok(anime);
+
     }
-    [HttpPost("create")]
+    [HttpPost("Create")]
     public async Task<IActionResult> Create([FromBody] CreateAnimeCommand request)
     {
         var anime = await _mediator.Send(request);
-        if (anime.anime is null) {
+        if (anime.anime is null)
+        {
             return BadRequest(new { Message = "Não foi possível criar o anime." });
         }
         return Ok(anime);
     }
+    [HttpGet("GetAnimeById")]
+    public async Task<IActionResult> GetAnimeById([FromQuery] GetAnimeQuery request)
+    {
+        var anime = await _mediator.Send(request);
 
+        return anime.anime is null ?
+        NotFound(new { Message = "anime não encontrado" }) :
+        Ok(anime);
+    }
+    [HttpGet("GetAnimesByKeywords")]
+    public async Task<IActionResult> GetAnimesByKeyWords([FromQuery] GetAnimesByKeyWordsQuery request)
+    {
+        var anime = await _mediator.Send(request);
+        return !anime.Animes.Any() ?
+        NotFound(new { Message = "animes com essa palavra-chaves não encontrado" }) :
+        Ok(anime);
+    }
+    [HttpGet("GetAnimesByIdDirector")]
+    public async Task<IActionResult> GetAnimesByIdDirector([FromQuery] GetAnimesByIdDirectorQuery request)
+    {
+        var anime = await _mediator.Send(request);
+        return !anime.Animes.Any() ?
+        NotFound(new { Message = "Esse diretor não tem animes" }) :
+        Ok(anime);
+    }
+
+    [HttpPut("Update")]
+    public async Task<IActionResult> Update([FromBody] UpdateAnimeCommand request)
+    {
+        var anime = await _mediator.Send(request);
+        return anime.anime is null ?
+        BadRequest(new { Message = "Não foi possível atualizar o anime" }) :
+        Ok(anime);
+    }
+
+    [HttpPut("Delete")]
+    public async Task<IActionResult> Delete([FromBody] DeleteAnimeCommand request)
+    {
+        /*var anime = await _mediator.Send(request);
+        return anime.anime is null ?
+        NotFound(new { Message = "Anime não encontrado" }) :
+        Ok(anime);*/
+        try
+        {
+            var deletedAnime = await _mediator.Send(request);
+            return Ok(deletedAnime);
+        }
+        catch (NotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
 
 }
