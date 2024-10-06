30537b43aa7ccbfdb0e96c48f60087da94b0c5a1
junio10 <junio.js977@gmail.com>
2024-10-06 09:27:49 -0300

    feat: adicionando classe GetAnimesFeature.cs que faz a comunicacao entre Api e Infraestructure

---
+++ Application/Features/AnimeFeatures/GetAnimesFeature.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Application.Interfaces.Repositories;
+using Domain.Entities;
+namespace Application.Features.AnimeFeature;
+
+public record GetAnimesQuery() : IRequest<GetAnimesResponse>;
+
+public record GetAnimesResponse(IEnumerable<Anime> animes);
+
+public class GetAnimesHandler : IRequestHandler<GetAnimesQuery, GetAnimesResponse>
+{
+    private readonly IAnimeRepository _animeRepository;
+    public GetAnimesHandler(IAnimeRepository animeRepository)
+    {
+        _animeRepository = animeRepository;
+    }
+
+    public async Task<GetAnimesResponse> Handle(GetAnimesQuery request, CancellationToken cancellationToken)
+    {
+
+        var animes = await _animeRepository.GetAllAnimes();
+
+        return new GetAnimesResponse(animes);
+    }
+}
+
